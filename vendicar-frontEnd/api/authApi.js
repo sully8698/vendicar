16c862d979bbed71567fc6d736596732b18067f5
Jonathan-Taylor319 <jontaylor319@gmail.com>
2025-03-01 15:27:17 -0500

    working and ready for production

--- vendicar-frontEnd/api/authApi.js
+++ vendicar-frontEnd/api/authApi.js
@@ -52,3 +52,31 @@ export async function getuserinfo(token) {
     const body = await basicFetch("http://localhost:8000/vendicar/accounts/",payload)
     return body
     }
+
+    export async function deleteuserinfo(token) {
+        const payload = {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${token}`,
+            },
+        }
+        const body = await basicFetch("http://localhost:8000/vendicar/accounts/delete",payload)
+        return body
+    }
+
+export async function updateuserinfo(token, updatedProfile) {
+    const payload = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${token}`,
+        },
+        body: JSON.stringify(updatedProfile),
+    }
+
+    const body = await basicFetch("http://localhost:8000/vendicar/accounts/update",payload)
+    
+    
+    return body
+}
--- vendicar-frontEnd/src/pages/LoggedHome.jsx
+++ vendicar-frontEnd/src/pages/LoggedHome.jsx
@@ -1,41 +1,166 @@
 import React, { useState, useEffect } from "react";
-import { getuserinfo } from "../../api/authApi";
+import { getuserinfo, updateuserinfo } from "../../api/authApi";
 import { useContext } from "react";
 import TokenContext from "../contexts/TokenContext";
+// import { useNavigate } from "react-router";
 
 export default function LoggedHome() {
+    // const navigate = useNavigate()
     const [userProfile, setUserProfile] = useState(null)
+    const [updatedProfile, setUpdatedProfile] = useState({})
     
     const userToken = useContext(TokenContext)
 
     useEffect(() => {
         const profileInfo = async () => {
-            
+            console.log(`useEffect token: ${userToken}`)
             const data = await getuserinfo(userToken)
+            
             setUserProfile(data)
-            console.log(data)
+            setUpdatedProfile(data.car_dealer_profile);  
         }
         profileInfo()
     }, [userToken])
 
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUpdatedProfile({
+            ...updatedProfile,
+            [name]: value
+        });
+    };
+    
+    
+    const handleUpdate = async (e) => {
+
+        const context = {
+            car_dealer_profile : {
+                business_name: updatedProfile.business_name,
+                state: updatedProfile.state, 
+                street_name: updatedProfile.street_name,
+                zip_code: updatedProfile.zip_code, 
+                city: updatedProfile.city,
+                phone_number: updatedProfile.phone_number,
+                business_email: updatedProfile.business_email,
+            }
+        }
+        console.log(updatedProfile)
+
+        e.preventDefault();
+        const response = await updateuserinfo(userToken, context);
+        const updatedData = await getuserinfo(userToken);
+        setUserProfile(updatedData);
+        setUpdatedProfile(updatedData.car_dealer_profile);
+       
+    };
+    
+
     return(
         <>
             {userProfile ? (
-                <div>
-                    <h1>{userProfile.car_dealer_profile.business_name}</h1>
-                    <ul>
-                        <li>{userProfile.car_dealer_profile.business_email}</li>
-                        <li>{userProfile.car_dealer_profile.phone_number}</li>
-                        <li>
-                            {userProfile.car_dealer_profile.street_name}<br></br>
-                            {userProfile.car_dealer_profile.city} {userProfile.car_dealer_profile.state} <br></br>
-                            {userProfile.car_dealer_profile.zip_code} 
-                        </li>
-                    </ul>
-                </div>
-            ) : (
+                <>
+                    <div>
+                        <h1>{userProfile.car_dealer_profile.business_name}</h1>
+                        <ul>
+                            <li>email: {userProfile.car_dealer_profile.business_email}</li>
+                            <li>phone number: {userProfile.car_dealer_profile.phone_number}</li>
+                            <li>
+                            Street:    {userProfile.car_dealer_profile.street_name}<br></br>
+                            City, State:    {userProfile.car_dealer_profile.city}, {userProfile.car_dealer_profile.state} <br></br>
+                            Zip:    {userProfile.car_dealer_profile.zip_code} 
+                            </li>
+                        </ul>
+                            {/* <button>Delete</button>  */}
+                    </div>
+                    
+                    <form onSubmit={ handleUpdate } >
+                    <div>
+                        <label htmlFor="business_name">Business Name:</label>
+                        <input
+                            type="text"
+                            id="business_name"
+                            name="business_name"
+                            value={ updatedProfile.business_name }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                    <div>
+                        <label htmlFor="state">State:</label>
+                        <input
+                            type="text"
+                            id="state"
+                            name="state"
+                            value={ updatedProfile.state }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                    <div>
+                        <label htmlFor="street_name">Street:</label>
+                        <input
+                            type="text"
+                            id="street_name"
+                            name="street_name"
+                            value={ updatedProfile.street_name }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                    <div>
+                        <label htmlFor="zip_code">Zip Code:</label>
+                        <input
+                            type="text"
+                            id="zip_code"
+                            name="zip_code"
+                            value={ updatedProfile.zip_code }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                    <div>
+                        <label htmlFor="city">City:</label>
+                        <input
+                            type="text"
+                            id="city"
+                            name="city"
+                            value={ updatedProfile.city }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                    <div>
+                        <label htmlFor="phone_number">Phone Number:</label>
+                        <input
+                            type="text"
+                            id="phone_number"
+                            name="phone_number"
+                            value={ updatedProfile.phone_number }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                    <div>
+                        <label htmlFor="business_email">Business Email:</label>
+                        <input
+                            type="email"
+                            id="business_email"
+                            name="business_email"
+                            value={ updatedProfile.business_email }
+                            onChange={ handleChange }
+                            required
+                            />  
+                    </div>
+                        <button type="submit" style={{ display: "block", margin: "auto"}}> Submit </button> 
+                </form>
+                    <br></br>
+                        {/* <button onClick={() => navigate('/')}>Cancel</button> */}
+                </>
+                          ) : (
                 <p>Loading user profile....</p>
             )}
+
         </>
        
 
